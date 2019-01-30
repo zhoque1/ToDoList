721f533ebea62f764997b075a5a43db0ad5238f9
Ziaul <zhoque@loopnet.com>
2019-01-30 13:52:38 -0800

    second check in

--- app/src/main/java/com/bignerdranch/android/todolist/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/todolist/MainActivity.java
@@ -1,13 +1,12 @@
 package com.bignerdranch.android.todolist;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 
 
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
