c0530f99e3f83b2984670a4278d6de3a2ec0af3b
Ziaul <zhoque@loopnet.com>
2019-01-30 17:07:31 -0800

    new comment

--- app/src/main/java/com/bignerdranch/android/todolist/SecondActivity.java
+++ app/src/main/java/com/bignerdranch/android/todolist/SecondActivity.java
@@ -18,6 +18,7 @@ public class SecondActivity extends Activity {
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(SecondActivity.this);
 
+                //this is for toasting
                 builder.setTitle(R.string.alert_title);
                 builder.setPositiveButton(R.string.ok, null);
                 builder.setMessage(R.string.alert_message);
